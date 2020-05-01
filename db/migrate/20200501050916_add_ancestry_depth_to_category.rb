9b8ca2da71a7ca477cdc723e2baa7ba736f01a75
naofumi-fujii <naofumi.fujii@gmail.com>
2020-05-01 14:09:54 +0900

    +    add_column :categories, :ancestry_depth, :integer, default: 0, null: false

--- db/migrate/20200501050916_add_ancestry_depth_to_category.rb
+++ db/migrate/20200501050916_add_ancestry_depth_to_category.rb
@@ -1,5 +1,5 @@
 class AddAncestryDepthToCategory < ActiveRecord::Migration[6.0]
   def change
-    add_column :categories, :ancestry_depth, :integer
+    add_column :categories, :ancestry_depth, :integer, default: 0, null: false
   end
 end
