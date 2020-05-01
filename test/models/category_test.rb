3257acb23d12a9ed18828868696066b64687ebf9
naofumi-fujii <naofumi.fujii@gmail.com>
2020-05-01 14:08:13 +0900

    +  test "nested category" do

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,17 @@
-require 'test_helper'
+require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "nested category" do
+    f = ->(node) { Category.create! :name => "wakuwaku", parent: node }
+    node = f.call(nil)
+    5.times.each do
+      node = f.call(node)
+    end
+
+    pp Category.all
+  end
 end
